87848a4178bced8e036152df2a2d21f647994228
TheRealRion <alyssambarker879@gmail.com>
2022-09-26 10:30:39 -0700

    - added an onCLick to the square div     - added a handleClick method to square     - passed index to square and to handleClick     - created handleGamePlay to square to index through functional props

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,14 @@
 import React from "react"
 
-const Square = () => {
+const Square = (props) => {
+
+  const handleClick= () => {
+    props.handleGamePlay(props.index)
+  }
+
   return (
     <>
-      <div className="square"></div>
+      <div className="square" onClick={handleClick}>{props.square}</div>
     </>
   )
 }
